74bdcfdcb9bbd0c63349f8dcaedfc291154f566e
깃ãhanseon97 <hanseon97@naver.com>
2023-12-20 15:56:34 +0900

    56565656565656

--- src/main/java/org/example/Member.java
+++ src/main/java/org/example/Member.java
@@ -1,14 +1,37 @@
 package org.example;
+
+
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
-@Getter
-@Setter
-@AllArgsConstructor
+//@Getter
+//@Setter
+//@AllArgsConstructor
 public class Member {
     private int memberId;
     private String userId;
     private String password;
     private String passwordConfirm;
+
+    Member(int memberId, String userId, String password, String passwordConfirm){
+        this.memberId = memberId;
+        this.userId = userId;
+        this.password = password;
+        this.passwordConfirm = passwordConfirm;
+    }
+
+    public int getMemberId(){
+        return this.memberId;
+    }
+    public  String getUserId(){
+        return this.userId;
+    }
+    public  String getPassword(){
+        return this.password;
+    }
+    public String getPasswordConfirm(){
+        return this.passwordConfirm;
+    }
+
 }
