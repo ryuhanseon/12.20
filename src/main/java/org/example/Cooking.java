fed7444abca477adf3bab0abbd0f916dd4ae0d46
깃ãhanseon97 <hanseon97@naver.com>
2023-12-20 17:40:24 +0900

    수정까지 완성본

--- src/main/java/org/example/Cooking.java
+++ src/main/java/org/example/Cooking.java
@@ -2,9 +2,9 @@ package org.example;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
-//@Getter
-//@Setter
-//@AllArgsConstructor
+@Getter
+@Setter
+@AllArgsConstructor
 
     public class Cooking {
         private int id;
@@ -16,45 +16,4 @@ import lombok.Setter;
         private String calories;
         private String tips;
         private String caution;
-
-        Cooking(int id, String ingredient, int time, String difficulty, String foodtype, String instruction, String calories, String tips
-        , String caution){
-            this.id = id;
-            this.ingredient =ingredient;
-            this.time =time;
-            this.difficulty =difficulty;
-            this.foodtype =foodtype;
-            this.instruction = instruction;
-            this.calories =calories;
-            this.tips =tips;
-            this.caution =caution;
-        }
-
-        public int getId(){
-            return this.id;
-        }
-        public String getIngredient(){
-            return this.getIngredient();
-        }
-        public int getTime(){
-            return this.time;
-        }
-        public String getDifficulty(){
-            return this.difficulty;
-        }
-        public String getFoodtype(){
-            return this.foodtype;
-        }
-        public String getInstruction(){
-            return this.instruction;
-        }
-        public String getCalories(){
-            return this.calories;
-        }
-        public String getTips(){
-            return this.tips;
-        }
-        public String getCaution(){
-            return this.caution;
-        }
     }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,10 @@
 package org.example;
-import java.lang.reflect.Member;
+
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
+
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
@@ -34,124 +35,174 @@ public class Main {
                     userId = sc.nextLine();
                     boolean isDuplecated = false;
 
-                        for (Member cookRecipe : memberList) {
-                            if (userId.equals(cookRecipe.getUserId())) {
-                                System.out.println("중복아이디가 존재합니다.");
-                                isDuplecated = true;
-                            }
-                        }
-                        if (!isDuplecated) {
-                            break;
+                    for (Member cookRecipe : memberList) {
+                        if (userId.equals(cookRecipe.getUserId())) {
+                            System.out.println("중복아이디가 존재합니다.");
+                            isDuplecated = true;
                         }
                     }
-                    while (true) {
-                        System.out.println("비밀번호를 입력해주세요: ");
-                        password = sc.nextLine();
-                        System.out.println("비밀번호를 확인합니다: ");
-                        passwordConfirm = sc.nextLine();
-                        if (password.equals(passwordConfirm)) {
-                            break;
-                        }
-                        System.out.println("일치하는 비밀번호가 없습니다.");
+                    if (!isDuplecated) {
+                        break;
+                    }
+                }
+                while (true) {
+                    System.out.println("비밀번호를 입력해주세요: ");
+                    password = sc.nextLine();
+                    System.out.println("비밀번호를 확인합니다: ");
+                    passwordConfirm = sc.nextLine();
+                    if (password.equals(passwordConfirm)) {
+                        break;
+                    }
+                    System.out.println("일치하는 비밀번호가 없습니다.");
+                }
+
+                Member cookRecipe = new Member(memberId, userId, password, now.toString());
+                memberList.add(cookRecipe);
+                System.out.println(userId + "님 회원가입을 환영합니다.");
+                memberId++;
+            } else if (command.equals("로그인")) {
+
+
+                Member rogin = null;
+                if (userInfo == null) {
+                    System.out.println("로그인 상태가 안닙니다.");
+                    continue;
+                }
+                System.out.printf("아이디를 입력해주세요: ");
+                String username = sc.nextLine();
+                System.out.printf("비밀번호를 입력해주세요: ");
+                String password = sc.nextLine();
+
+                for (Member cookRecipe : memberList) {
+                    if (username.equals(cookRecipe.getUserId())) {
+                        rogin = cookRecipe;
+                        break;
+                    }
+                }
+                if (rogin == null) {
+                    System.out.println("로그인이 되지 않습니다.");
+                    continue;
+                }
+
+                if (rogin.getPassword().equals(password) == false) {
+                    System.out.println("일치하는 비밀번호가 없습니다.");
+                    continue;
+                }
+                userInfo = rogin;
+                System.out.println(username + "님 로그인이 완료되었습니다.");
+
+
+            } else if (command.equals("로그아웃")) {
+
+                if (userInfo == null) {
+                    System.out.println("로그아웃 상태입니다.");
+                    continue;
+                }
+
+            } else if (command.equals("레시피 등록")) {
+
+                System.out.println("재료: ");
+                String ingredient = sc.nextLine();
+                System.out.println("소요시간: ");
+                int time = Integer.parseInt(sc.nextLine());
+                System.out.println("난이도: ");
+                String difficulty = sc.nextLine();
+                System.out.println("음식종류: ");
+                String foodtype = sc.nextLine();
+                System.out.println("조리방법: ");
+                String instruction = sc.nextLine();
+                System.out.println("칼로리: ");
+                String calories = sc.nextLine();
+                System.out.println("팁: ");
+                String tips = sc.nextLine();
+                System.out.println("주의사항: ");
+                String caution = sc.nextLine();
+
+
+                Cooking cooker = new Cooking(chefId, ingredient, time, difficulty, foodtype, instruction, calories, tips, caution);
+                cookerList.add(cooker);
+
+                chefId++;
+            } else if (command.equals("목록")) {
+                System.out.println("재료 / 소요시간 / 난이도 / 음식종류 / 조리방법 / 칼로리 / 팁 / 주의사항");
+                System.out.println("--------------------");
+
+                for (Cooking cooking : cookerList) {
+                    System.out.printf("%s, %d, %s, %s, %s, %s, %s, %s\n", cooking.getIngredient(), cooking.getTime(), cooking.getDifficulty(),
+                            cooking.getFoodtype(), cooking.getInstruction(), cooking.getCalories(), cooking.getTips(), cooking.getCaution());
+                }
+
+
+            } else if (command.equals("삭제")) {
+                System.out.println("삭제할 Id를 입력해 주세요: ");
+                int deleteId = Integer.parseInt(sc.nextLine());
+                for (int i = 0; i < cookerList.size(); i++) {
+                    if (deleteId == cookerList.get(i).getId()) {
+                        cookerList.remove(i);
+                        System.out.println(deleteId + "값이 지워집니다.");
+                    }
+                }
+            } else if (command.equals("수정")) {
+                System.out.println("수정할 값을 입력해주세요: ");
+                String modified = sc.nextLine();
+                for (int j = 0; j < cookerList.size(); j++){
+                    if (modified.equals(String.valueOf(cookerList.get(j).getId())) ){
+                        System.out.printf("기존재료: %s \n", cookerList.get(j).getIngredient());
+                        String ingredient = sc.nextLine();
+                        cookerList.get(j).setIngredient(ingredient);
+                        System.out.println("재료가 수정되었습니다.");
+                    }
+                    if (modified.equals(String.valueOf(cookerList.get(j).getId())) ){
+                        System.out.printf("기존시간: %s \n", cookerList.get(j).getTime());
+                        int time = Integer.parseInt(sc.nextLine());
+                        cookerList.get(j).setTime(time);
+                        System.out.println("시간이 수정되었습니다.");
+                    }
+                    if (modified.equals(String.valueOf(cookerList.get(j).getId())) ){
+                        System.out.printf("기존난이도: %s \n", cookerList.get(j).getDifficulty());
+                        String difficulty = sc.nextLine();
+                        cookerList.get(j).setDifficulty(difficulty);
+                        System.out.println("난이도가 수정되었습니다.");
+                    }
+                    if (modified.equals(String.valueOf(cookerList.get(j).getId())) ){
+                        System.out.printf("기존음식종류: %s \n", cookerList.get(j).getFoodtype());
+                        String foodtype = sc.nextLine();
+                        cookerList.get(j).setFoodtype(foodtype);
+                        System.out.println("음식종류가 수정되었습니다.");
+                    }
+                    if (modified.equals(String.valueOf(cookerList.get(j).getId())) ){
+                        System.out.printf("기존조리방법: %s \n", cookerList.get(j).getInstruction());
+                        String instruction = sc.nextLine();
+                        cookerList.get(j).setInstruction(instruction);
+                        System.out.println("조리방법이 수정되었습니다.");
+                    }
+                    if (modified.equals(String.valueOf(cookerList.get(j).getId())) ){
+                        System.out.printf("기존칼로리: %s \n", cookerList.get(j).getCalories());
+                        String calories = sc.nextLine();
+                        cookerList.get(j).setCalories(calories);
+                        System.out.println("칼로리가 수정되었습니다.");
+                    }
+                    if (modified.equals(String.valueOf(cookerList.get(j).getId())) ){
+                        System.out.printf("기존팁: %s \n", cookerList.get(j).getTips());
+                        String tips = sc.nextLine();
+                        cookerList.get(j).setTips(tips);
+                        System.out.println("팁 수정되었습니다.");
+                    }
+                    if (modified.equals(String.valueOf(cookerList.get(j).getId())) ){
+                        System.out.printf("기존주의사항: %s \n", cookerList.get(j).getCaution());
+                        String caution = sc.nextLine();
+                        cookerList.get(j).setCaution(caution);
+                        System.out.println("주의사항이 수정되었습니다.");
                     }
 
-                    Member cookRecipe = new Member(memberId, userId, password, now.toString());
-                    memberList.add(cookRecipe);
-                    System.out.println(userId + "님 회원가입을 환영합니다.");
-                    memberId++;
-                } else if (command.equals("로그인")) {
+                }
+                System.out.println(modified + "값이 수정되었습니다.");
+
+            }
 
 
-                        Member rogin = null;
-                        if (userInfo == null) {
-                            System.out.println("로그인 상태가 안닙니다.");
-                            continue;
-                        }
-                        System.out.printf("아이디를 입력해주세요: ");
-                        String username = sc.nextLine();
-                        System.out.printf("비밀번호를 입력해주세요: ");
-                        String password = sc.nextLine();
+        }
+        sc.close();
 
-                            for (Member cookRecipe : memberList) {
-                                if (username.equals(cookRecipe.getUserId())) {
-                                    rogin = cookRecipe;
-                                    break;
-                                }
-                            }
-                            if (rogin == null) {
-                                System.out.println("로그인이 되지 않습니다.");
-                                continue;
-                            }
-
-                                if (rogin.getPassword().equals(password) == false) {
-                                    System.out.println("일치하는 비밀번호가 없습니다.");
-                                    continue;
-                                }
-                                userInfo = rogin;
-                                System.out.println(username + "님 로그인이 완료되었습니다.");
-
-
-                            } else if (command.equals("로그아웃")) {
-
-                                    if (userInfo == null) {
-                                        System.out.println("로그아웃 상태입니다.");
-                                        continue;
-                                    }
-
-                                } else if (command.equals("레시피 등록")) {
-
-                                    System.out.println("재료: ");
-                                    String ingredient = sc.nextLine();
-                                    System.out.println("소요시간: ");
-
-                                    int time = Integer.parseInt(sc.nextLine());
-                                    System.out.println("난이도: ");
-                                    String difficulty = sc.nextLine();
-                                    System.out.println("음식종류: ");
-                                    String foodtype = sc.nextLine();
-                                    System.out.println("조리방법: ");
-                                    String instruction = sc.nextLine();
-                                    System.out.println("칼로리: ");
-                                    String calories = sc.nextLine();
-                                    System.out.println("팁: ");
-                                    String tips = sc.nextLine();
-                                    System.out.println("주의사항: ");
-                                    String caution = sc.nextLine();
-
-
-                                    Cooking cooker = new Cooking(chefId, ingredient, time, difficulty, foodtype, instruction, calories, tips, caution);
-                                    cookerList.add(cooker);
-
-                                    chefId++;
-                                } else if (command.equals("목록")) {
-                                    System.out.println("재료 / 소요시간 / 난이도 / 음식종류 / 조리방법 / 칼로리 / 팁 / 주의사항");
-                                    System.out.println("--------------------");
-
-                                        for (Cooking cooking : cookerList) {
-                                            System.out.printf("%s, %d, %s, %s, %s, %s, %s, %s\n", cooking.getIngredient(), cooking.getTime(), cooking.getDifficulty(),
-                                                    cooking.getFoodtype(), cooking.getInstruction(), cooking.getCalories(), cooking.getTips(), cooking.getCaution());
-                                        }
-
-
-                                            } else if (command.equals("삭제")) {
-                                                System.out.println("삭제할 Id를 입력해 주세요: ");
-                                                int deleteId = Integer.parseInt(sc.nextLine());
-                                                for (int i = 0; i < cookerList.size(); i++) {
-                                                    if (deleteId == cookerList.get(i).getTime()) {
-                                                        cookerList.remove(i);
-                                                        System.out.println(deleteId + "값이 지워집니다.");
-                                                    }
-
-                                                }
-
-
-
-                                            }
-
-
-
-                                    }
-                                    sc.close();
-
-                                }
-                            }
+    }
+}
